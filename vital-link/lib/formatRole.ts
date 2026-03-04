141c84e864f0ff0d9920669ed996569fb30f7501
davidmuir1999 <david.bme.muir2017@gmail.com>
2026-03-04 16:35:56 +0000

    remove unused functions

--- vital-link/lib/formatRole.ts
+++
@@ -1,8 +0,0 @@
-export default function formatRole(role:string) {
-    let split = role.split("_")
-    let formatted = ""
-    for (let i = 0; i < split.length; i++){
-        formatted += split[i].charAt(0).toUpperCase() + split[i].slice(1).toLowerCase() + " "
-    }
-    return formatted
-}
