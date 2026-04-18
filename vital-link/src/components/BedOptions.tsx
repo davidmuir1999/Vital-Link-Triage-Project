9fff6afb4d99c53d2d249e2a425a90d9c17ef643
davidmuir1999 <david.bme.muir2017@gmail.com>
2026-04-18 13:28:46 +0100

    Fix sorting function of patient clinical notes

--- vital-link/src/components/BedOptions.tsx
+++ vital-link/src/components/BedOptions.tsx
@@ -473,8 +473,8 @@ export default function BedOptions({ bed }: { bed: BedWithPatientData }) {
                   }))
                   .sort(
                     (a, b) =>
-                      new Date(b.timestamp).getTime() -
-                      new Date(a.timestamp).getTime()
+                      new Date(a.timestamp).getTime() -
+                      new Date(b.timestamp).getTime()
                   )}
               />
             </div>
